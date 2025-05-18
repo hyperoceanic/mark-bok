01ab47aa494e24345c6089da451b8f6f902b3d2a
Mark Smith <hyperoceanic@users.noreply.github.com>
2025-05-18 17:19:18 +0100

    rename sidebar

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -68,7 +68,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'mark.bok',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
@@ -76,9 +76,9 @@ const config: Config = {
       items: [
         {
           type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
+          sidebarId: 'mainSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'bok',
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
